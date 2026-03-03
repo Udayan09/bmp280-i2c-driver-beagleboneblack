036b3e71059af5d1f90e80f2be10115d6b7dc2f5
FIXED-TERM Borah Udayan (BGSW/PJ-ETA-P) <ruf3kor@bosch.com>
2026-03-03 16:03:48 +0530

    Adding c file for the driver

---
+++ bmp280.c
@@ -0,0 +1,11 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/i2c.h>
+#include <linux/mutex.h>
+
+
+
+
+MODULE_AUTHOR("Udayan Borah");
+MODULE_DESCRIPTION("BMP280 Driver");
+MODULE_LICENSE("GPL");
